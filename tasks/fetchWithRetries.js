a3c88e72f19ec6366eaa2dbe2a6c68092409b31b
Danil Larin <danil.l@blink-market.com>
2024-03-10 21:29:01 +0200

    fetchWithRetries.js

---
+++ tasks/fetchWithRetries.js
@@ -0,0 +1,19 @@
+async function fetchWithRetries(retries = 0, ...fetchArgs) {
+    let result;
+
+    for (let attempt = 0; attempt <= retries; attempt++) {
+        const response = await fetch(...fetchArgs)
+
+        if (response.ok) {
+            result = await response.json()
+            break;
+        }
+    }
+
+    return result
+}
+
+
+(async () => {
+    console.log("### result", await fetchWithRetries(5, 'https://jsonplaceholder.typicode.com/posts'))
+})()
